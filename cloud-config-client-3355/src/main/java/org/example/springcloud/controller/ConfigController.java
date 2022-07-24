664092511ba30dc619eea8525195d2dc455ac8e3
tindal-966 <qinghuang1995@gmail.com>
2022-07-24 18:28:04 +0800

    feat: SpringCloud Config client noReboot manually update
    手动非重启更新 Config Client 配置内容

--- cloud-config-client-3355/src/main/java/org/example/springcloud/controller/ConfigController.java
+++ cloud-config-client-3355/src/main/java/org/example/springcloud/controller/ConfigController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  * 实现热加载 自动获取刷新内容
  */
 @RestController
-@RefreshScope
+@RefreshScope // 动态更新 client
 public class ConfigController {
 
     @Value("${config.info}") // 这个是 config server 中某个（具体哪个取决与本项目配置文件的配置，label/name-profile.yml）配置文件的一个属性
